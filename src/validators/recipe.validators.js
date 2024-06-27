7bce273655d64266fb869f9ed443c6767fe9faeb
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-27 09:33:46 +0300

    recipe validators added

--- src/validators/recipe.validators.js
+++ src/validators/recipe.validators.js
@@ -1,5 +1,26 @@
 const joi = require("joi");
 
+const validator = (validationSchema) => async (req, res, next) => {
+  try {
+    const result = await validationSchema.validate(req.body);
+    if (result.error) {
+      return res.status(400).json({
+        status: "error",
+        message: "Validation error",
+        data: result.error
+      })
+    }
+
+    next();
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: "Validation error",
+      data: error,
+    })
+  }
+};
+
 const schema = joi.object({
   title: joi.string().max(50).trim(),
   description: joi.string().max(256).lowercase().trim(),
@@ -8,7 +29,22 @@ const schema = joi.object({
       name: joi.string(),
       quantity: joi.string()
     })
-  )
+  ),
+  instructions: joi.array().items(
+    joi.object({
+      stepNumber: joi.number().required(),
+      description: joi.string(),
+    })
+  ),
+  prepTimeInMinutes: joi.number().required(),
+  cookTimeInMinutes: joi.number().required(),
+  numberOfServings: joi.number().required(),
+  category: joi.string().required(),
+  cuisine: joi.string().required(),
+  difficulty: joi.string().required(),
+  image: joi.string(),
 });
 
 schema.validate({});
+
+module.exports = { validateCreateRecipe: validator(schema)};
